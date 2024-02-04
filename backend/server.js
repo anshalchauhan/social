bbfad8a53705d1daff178f9c58c11cbf7da36a82
anshal <anshalchauhan889@gmail.com>
2024-02-04 19:43:43 +0530

    database and express completed

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+// http module to create an http server
+const http = require("http");
+
+// ---------------Dotenv---------------
+// Dotenv loads environment variables from a .env file into process.env.
+const dotenv = require("dotenv");
+
+dotenv.config({ path: ".env" });
+
+// ---------------Mongoose---------------
+// Mongoose is an object modeling tool from MongoDB and Node.js
+const mongoose = require("mongoose");
+
+// Creating the DB String
+const DB = process.env.MONGO_URI.replace(
+  "<password>",
+  process.env.MONGO_PASSWORD
+);
+
+// Connecting to DB
+mongoose.connect(DB).then(() => {
+  console.log("DB Connection successful!");
+});
+
+// ---------------App Module---------------
+// App module contains all the express logic
+const app = require("./app");
+
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
+);
