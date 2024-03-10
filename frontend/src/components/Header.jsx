94353f75e65304673ec559edbf892046d73e5e9d
anshal <anshalchauhan889@gmail.com>
2024-03-10 23:55:44 +0530

    latest commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,9 @@
 // UI
-import {
-  IconButton,
-  Box,
-  Flex,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box, Flex, useColorMode } from "@chakra-ui/react";
 import Logo from "../assets/icons/Logo";
-import { MdDarkMode, MdLightMode } from "react-icons/md";
-import { IoMdHome } from "react-icons/io";
-import { FaCircleUser } from "react-icons/fa6";
-
-// Components
-import LogoutButton from "../components/auth/LogoutButton";
-import CreatePost from "../components/CreatePost";
-
-// React-Redux
-import { useSelector } from "react-redux";
-
-// Navigation
-import { useNavigate } from "react-router-dom";
 
 function Header() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const { user, username } = useSelector((state) => state.app);
-  const navigate = useNavigate();
+  const { colorMode } = useColorMode();
 
   return (
     <Flex
